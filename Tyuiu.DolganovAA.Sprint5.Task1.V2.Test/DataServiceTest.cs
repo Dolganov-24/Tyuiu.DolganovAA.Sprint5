63a2eaded7635b75a50eacdde8a82bccff860174
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 04:49:52 +0300

    Task1

---
+++ Tyuiu.DolganovAA.Sprint5.Task1.V2.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.DolganovAA.Sprint5.Task1.V2.Lib;
+namespace Tyuiu.DolganovAA.Sprint5.Task1.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint5.Task1.V2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.DolganovAA.Sprint5.Task1.V2.Lib;
+namespace Tyuiu.DolganovAA.Sprint5.Task1.V2
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+
+        Console.Title = "Спринт #5 | Выполнил: Долганов А. А. | СМАРТБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Долганов Александр Анатольевич |  СМАРТБ-24-1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        int startValue = -5;
+        int stopValue = 5;
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(startValue, stopValue);
+        Console.WriteLine("Файл " + res);
+        Console.WriteLine("Cоздан");
+        Console.ReadKey();
+    }
+}
