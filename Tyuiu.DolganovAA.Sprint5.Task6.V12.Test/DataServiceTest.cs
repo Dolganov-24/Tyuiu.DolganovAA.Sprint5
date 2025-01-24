99c84b5a332f25e8db1439c8698bd0ec975757c8
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 05:24:12 +0300

    Task6

---
+++ Tyuiu.DolganovAA.Sprint5.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.DolganovAA.Sprint5.Task6.V12.Lib;
+namespace Tyuiu.DolganovAA.Sprint5.Task6.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint5.Task6.V12/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DolganovAA.Sprint5.Task6.V12.Lib;
+namespace Tyuiu.DolganovAA.Sprint5.Task6.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask6V12.txt");
+
+            Console.WriteLine(ds.LoadFromDataFile(path));
+
+        }
+    }
+}
