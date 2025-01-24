8732aa1c98eb3d67ad374f04d2d9f48d779703d9
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 04:59:23 +0300

    Task3

---
+++ Tyuiu.DolganovAA.Sprint5.Task3.V5.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.DolganovAA.Sprint5.Task3.V5.Lib;
+namespace Tyuiu.DolganovAA.Sprint5.Task3.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint5.Task3.V5/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.DolganovAA.Sprint5.Task3.V5.Lib;
+namespace Tyuiu.DolganovAA.Sprint5.Task3.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(2);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+        }
+    }
+}
