e76f080aafd0d960aeadb67e9c67ef09085dd647
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2025-01-24 04:26:32 +0300

    Add project files.

---
+++ Tyuiu.DolganovAA.Sprint5.Task0.V9/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DolganovAA.Sprint5.Task0.V9.Lib;
+namespace Tyuiu.DolganovAA.Sprint5.Task0.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+
+            DataService ds = new DataService();
+            Console.Write("x = " + x);//
+            Console.WriteLine();
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+
+        }
+    }
+}
